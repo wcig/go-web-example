cf17698467e2da701e5450ac06c827cd53356206
wcig <job.yangbo@gmail.com>
2021-10-02 13:33:28 +0800

    feat: test

--- test/boot_test.go
+++ test/boot_test.go
@@ -1,84 +1,28 @@
 package test
 
 import (
-	"fmt"
+	"go-app/library/boot"
 	"go-app/library/log"
-	"go-app/library/util/json"
-	"go-app/library/util/yaml"
-	"strings"
+	"os"
+	"path/filepath"
 	"testing"
-
-	"github.com/spf13/viper"
 )
 
-// func init() {
-// 	wd, _ := os.Getwd()
-// 	_ = os.Chdir(filepath.Dir(wd))
-// }
-//
-// func testBootRun() {
-// 	boot.Run("dev")
-// }
-//
-// func testAppInit() {
-// 	config.Init("dev")
-// 	log.Init(config.Get().Logger)
-// }
-
-type Config struct {
-	Logging log.LogConfig `yaml:"logging" json:"logging"`
+func init() {
+	wd, _ := os.Getwd()
+	_ = os.Chdir(filepath.Dir(wd))
 }
 
-func Test(t *testing.T) {
-	var conf Config
-	err := yaml.LoadFromFile(&conf, "../config/config-dev.yml")
-	fmt.Println(err)
-	json.PrintJsonPretty(conf)
+func initBootStarter() {
+	boot.Register(&log.LogStarter{})
 }
 
-func Test2(t *testing.T) {
-	conf := viper.New()
-	conf.SetConfigType("yaml")
-	conf.SetConfigFile("../config/config-dev.yml")
-	if err := conf.ReadInConfig(); err != nil {
-		panic(err)
-	}
-
-	var cfg Config
-	if err := conf.Unmarshal(&cfg); err != nil {
-		panic(err)
-	}
-	json.PrintJsonPretty(cfg)
+func testBootRun() {
+	initBootStarter()
+	boot.Run("dev")
 }
 
-func Test3(t *testing.T) {
-	str := `{
-  "logging": {
-    "enabled": true,
-    "path": "/tmp/log",
-    "stdout": true,
-    "access": true,
-    "root": {
-      "file_name": "info.log",
-      "max_size": 1,
-      "max_age": 2,
-      "max_backups": 3,
-      "compress": true,
-      "level": "debug"
-    }
-  }
-}`
-
-	conf := viper.New()
-	conf.SetConfigType("json")
-	err := conf.ReadConfig(strings.NewReader(str))
-	if err != nil {
-		panic(err)
-	}
-
-	var cfg Config
-	if err := conf.Unmarshal(&cfg); err != nil {
-		panic(err)
-	}
-	json.PrintJsonPretty(cfg)
+func TestLog(t *testing.T) {
+	testBootRun()
+	log.Info("ok")
 }
